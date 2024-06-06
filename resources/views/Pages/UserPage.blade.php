102a5a8613df86deddd6700072672270422327d4
Vitalij <krinickij.v.a@gmail.com>
2024-06-06 11:15:11 +0300

    feat: created userPage and controller for it
    Создана страница пользователя с контроллером

---
+++ resources/views/Pages/UserPage.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-5xl">Страница пользователя {{$nickName}} </h1>
+        <div class="grid grid-cols-2 gap-3 pt-4">
+            <div class="">
+                <h3>Личные данные</h3>
+                <ul>
+                    <li>Имя:{{$name}} </li>
+                    <li>Фамилия :{{$surname}}</li>
+                    <li>Отчество:{{$family}}</li>
+                    <li>Дата рождения:{{$burthday}}</li>
+                    <li>Права:{{$driverLicense}} </li>
+                    <?=(isset($auto) && $auto == true) ? '<li>Имеется авто</li>' : ''?>
+                </ul>
+            </div>
+            <div class=" flex gap-3 flex-col">
+                <h2>Медицинские данные</h2>
+                <div class="">
+                    <h4>Противопоказания:</h4>
+                    <p>{{$contraindications}}</p>
+                </div>
+                <div class="">
+                    <h4>Аллергии:</h4>
+                    <p>{{$allergies}}</p>
+                </div>
+                <div class="">
+                    <h4>Экстренные контакты:</h4>
+                    <p>{{$extraContacts}}</p>
+                </div>
+            </div>
+
+        </div>
+        <div class=" mt-5">
+            <h3 class="text">Дополнительная информация</h3>
+            <div class="">
+                {{$about}}
+            </div>
+        </div>
+
+    </div>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UsersIndexController;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +14,4 @@ Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/users', [UsersIndexController::class, 'index'])->name('users');
+Route::get('/user/01' , [UserController::class, 'index'])->name('user01');
